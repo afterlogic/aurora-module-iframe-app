c91b79a892d34c31ac96034701cf9215d995a5eb
AfterLogic Support <support@afterlogic.com>
2016-11-08 18:03:46 +0300

    Iframe App

---
+++ module.php
@@ -0,0 +1,58 @@
+<?php
+
+class IframeAppModule extends AApiModule
+{
+	public $oApiIframeAppManager = null;
+	
+	public function init() 
+	{
+		$this->oApiIframeAppManager = $this->GetManager();
+		
+		$this->setObjectMap('CUser', array(
+				'EnableModule' => array('bool', true)
+			)
+		);
+	}
+	
+	/**
+	 * Obtaines list of module settings for authenticated user.
+	 * 
+	 * @return array
+	 */
+	public function GetSettings()
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::Anonymous);
+		
+		$oUser = \CApi::getAuthenticatedUser();
+		if (!empty($oUser) && $oUser->Role === \EUserRole::NormalUser)
+		{
+			return array(
+				'EnableModule' => $oUser->{$this->GetName().'::EnableModule'}
+			);
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Updates module settings.
+	 * 
+	 * @param boolean $EnableModule indicates if user turned module on.
+	 * @return boolean
+	 */
+	public function UpdateSettings($EnableModule)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+		
+		$iUserId = \CApi::getAuthenticatedUserId();
+		if (0 < $iUserId)
+		{
+			$oCoreDecorator = \CApi::GetModuleDecorator('Core');
+			$oUser = $oCoreDecorator->GetUser($iUserId);
+			$oUser->{$this->GetName().'::EnableModule'} = $EnableModule;
+			$oCoreDecorator->UpdateUserObject($oUser);
+		}
+		return true;
+	}
+	
+}
