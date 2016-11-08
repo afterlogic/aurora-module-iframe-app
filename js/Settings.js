ba5df5160b8b3af17d0ce9d9a21a184f30d51c1e
AfterLogic Support <support@afterlogic.com>
2016-11-08 16:33:14 +0300

    Iframe Sample

---
+++ js/Settings.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var ko = require('knockout');
+
+module.exports = {
+	ServerModuleName: 'IframeSample',
+	HashModuleName: 'iframesample',
+	
+	/**
+	 * Setting indicates if module is enabled by user or not.
+	 * The Core subscribes to this setting changes and if it is **true** displays module tab in header and its screens.
+	 * Otherwise the Core doesn't display module tab in header and its screens.
+	 */
+	enableModule: ko.observable(false),
+	
+	/**
+	 * Initializes settings of the module.
+	 * 
+	 * @param {Object} oAppDataSection Sample module section in AppData.
+	 */
+	init: function (oAppDataSection) {
+		if (oAppDataSection)
+		{
+			this.enableModule(!!oAppDataSection.EnableModule);
+		}
+	},
+	
+	/**
+	 * Updates module settings after editing.
+	 * 
+	 * @param {boolean} bEnableModule New value of setting 'EnableModule'
+	 */
+	update: function (bEnableModule) {
+		this.enableModule(bEnableModule);
+	}
+};
